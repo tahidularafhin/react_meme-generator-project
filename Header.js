40ffdf0a8d3b92eb3e98f59aeb9305154e2eecef
Tahidul Arafhin <48841466+tahidularafhin@users.noreply.github.com>
2021-07-29 12:38:22 +0200

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                alt="Problem?" width="460" height="345"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
